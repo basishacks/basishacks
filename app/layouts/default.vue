9629f09f265f7afb61e6ad7b02e547ec5dd019a3
david-why <david_why@outlook.com>
2025-11-10 13:22:28 +0800

    initial commit

---
+++ app/layouts/default.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import type { NavigationMenuItem } from '@nuxt/ui'
+
+const navItems = computed<NavigationMenuItem[]>(() => [
+  {
+    label: 'Home',
+    to: '/',
+    icon: 'i-material-symbols-home-outline',
+  },
+])
+</script>
+
+<template>
+  <div>
+    <UHeader class="mb-4">
+      <template #title>
+        <span class="text-primary">basishacks_2025</span>
+      </template>
+
+      <UNavigationMenu :items="navItems" />
+
+      <template #body>
+        <UNavigationMenu
+          :items="navItems"
+          orientation="vertical"
+          class="-mx-2.5"
+        />
+      </template>
+    </UHeader>
+
+    <UMain>
+      <UContainer>
+        <slot />
+      </UContainer>
+    </UMain>
+  </div>
+</template>
---
+++ app/pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1 class="text-4xl font-mono-bold">Welcome to basishacks_2025!</h1>
+    <p>123123</p>
+  </div>
+</template>
