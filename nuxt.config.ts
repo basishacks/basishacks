8a5ce7f1d14ceac225370ddfabbbc56c904571b2
david-why <david_why@outlook.com>
2025-11-11 17:35:58 +0800

    add nuxt-auth-utils module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,22 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['nitro-cloudflare-dev', '@nuxt/eslint', '@nuxt/ui'],
+  modules: [
+    'nitro-cloudflare-dev',
+    '@nuxt/eslint',
+    '@nuxt/ui',
+    'nuxt-auth-utils',
+  ],
   css: ['~/assets/css/main.css'],
   experimental: {
     asyncContext: true,
+  },
+  devServer: {
+    port: 24598,
+  },
+  vite: {
+    server: {
+      allowedHosts: true,
+    },
   },
 })
