5b1549b90352dc015c73560ffb3eb182102ff9ff
david-why <david_why@outlook.com>
2025-11-11 17:24:25 +0800

    add team and user tables

--- sql/init.sql
+++ sql/init.sql
@@ -1,6 +1,4 @@
-DROP TABLE IF EXISTS hackathon;
-
-CREATE TABLE hackathon (
+CREATE TABLE IF NOT EXISTS hackathon (
     id INTEGER PRIMARY KEY CHECK (id = 1),
     status TEXT NOT NULL CHECK (status IN ('not_started', 'in_progress', 'voting', 'finished', 'paused')),
     start_timestamp INTEGER NOT NULL,
@@ -10,3 +8,20 @@ CREATE TABLE hackathon (
     results_open_timestamp INTEGER NOT NULL,
     theme TEXT
 );
+
+CREATE TABLE IF NOT EXISTS teams (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    project_name TEXT NOT NULL DEFAULT '',
+    project_description TEXT NOT NULL DEFAULT '',
+    project_demo_url TEXT,
+    project_repo_url TEXT
+);
+
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    email TEXT NOT NULL UNIQUE,
+    team_id INTEGER,
+    FOREIGN KEY (team_id) REFERENCES teams(id)
+);
+CREATE INDEX idx_users_email ON users (email);
+CREATE INDEX idx_users_team_id ON users (team_id);
