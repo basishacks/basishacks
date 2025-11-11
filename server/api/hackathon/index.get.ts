7ed198478f59d67d8bafa582fc5eecf04258c8ed
david-why <david_why@outlook.com>
2025-11-11 22:39:09 +0800

    add hiding theme before hackathon starts

--- server/api/hackathon/index.get.ts
+++ server/api/hackathon/index.get.ts
@@ -8,6 +8,8 @@ export default defineEventHandler(async (event) => {
     })
   }
 
+  const showTheme = hackathon.status !== 'not_started'
+
   return {
     status: hackathon.status,
     start_timestamp: hackathon.start_timestamp,
@@ -15,7 +17,7 @@ export default defineEventHandler(async (event) => {
     voting_start_timestamp: hackathon.voting_start_timestamp,
     voting_end_timestamp: hackathon.voting_end_timestamp,
     results_open_timestamp: hackathon.results_open_timestamp,
-    theme_name: hackathon.theme_name,
-    theme_description: hackathon.theme_description,
+    theme_name: showTheme ? hackathon.theme_name : null,
+    theme_description: showTheme ? hackathon.theme_description : null,
   }
 })
