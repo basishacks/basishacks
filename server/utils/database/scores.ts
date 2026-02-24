e8eca5293facc0262127f109e0b4d51312844df7
jollyroger182 <hello@jollyy.dev>
2026-02-24 09:08:26 +0800

    oops

--- server/utils/database/scores.ts
+++ server/utils/database/scores.ts
@@ -5,7 +5,7 @@ export async function createTeamScores(
   scores: Pick<TeamScores, 'team_id' | 'judge_user_id' | 'scores' | 'reasoning'>
 ) {
   return (await event.context.cloudflare.env.DB.prepare(
-    'INSERT INTO team_scores(team_id, judge_user_id, reasoning, scores) VALUES(?, ?, ?) RETURNING *'
+    'INSERT INTO team_scores(team_id, judge_user_id, reasoning, scores) VALUES(?, ?, ?, ?) RETURNING *'
   )
     .bind(scores.team_id, scores.judge_user_id, scores.reasoning, scores.scores)
     .first<TeamScores>())!
